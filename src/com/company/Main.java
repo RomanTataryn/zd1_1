8d087f406aef6048d1fede050b0385af3f48a17f
Roman Tataryn <rtataryn@gmail.com>
2021-09-22 21:13:13 +0300

    Add files via upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,29 +3,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        byte a=20, b=10;
-        short a1=31500, b1=-2187;
-        long a2=622377233699l, b2= 2511111145l;
-        double a3= 1010.5055555, b3=205.01822111111111;
-        float a4= (float) 101.27, b4= (float) 5552458622.55;
-        System.out.println ("byte");
-        System.out.println (a+b);        System.out.println (a-b);
-        System.out.println (a*b);        System.out.println (a/b);
+        byte a = 20, b = 10;
+        short a1 = 31500, b1 = -2187;
+        long a2 = 622377233699l, b2 = 2511111145l;
+        double a3 = 1010.5055555, b3 = 205.01822111111111;
+        float a4 = (float) 101.27, b4 = (float) 5552458622.55;
+        System.out.println("byte");
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
 
-        System.out.println ("short");
-        System.out.println (a1+b1);        System.out.println (a1-b1);
-        System.out.println (a1*b1);        System.out.println (a1/b1);
+        System.out.println("short");
+        System.out.println(a1 + b1);
+        System.out.println(a1 - b1);
+        System.out.println(a1 * b1);
+        System.out.println(a1 / b1);
 
-        System.out.println ("long");
-        System.out.println (a2+b2);        System.out.println (a2-b2);
-        System.out.println (a2*b2);        System.out.println (a2/b2);
+        System.out.println("long");
+        System.out.println(a2 + b2);
+        System.out.println(a2 - b2);
+        System.out.println(a2 * b2);
+        System.out.println(a2 / b2);
 
-        System.out.println ("double");
-        System.out.println (a3+b3);        System.out.println (a3-b3);
-        System.out.println (a3*b3);        System.out.println (a3/b3);
+        System.out.println("double");
+        System.out.println(a3 + b3);
+        System.out.println(a3 - b3);
+        System.out.println(a3 * b3);
+        System.out.println(a3 / b3);
 
-        System.out.println ("float");
-        System.out.println (a4+b4);        System.out.println (a4-b4);
-        System.out.println (a4*b4);        System.out.println (a4/b4);
+        System.out.println("float");
+        System.out.println(a4 + b4);
+        System.out.println(a4 - b4);
+        System.out.println(a4 * b4);
+        System.out.println(a4 / b4);
     }
 }
